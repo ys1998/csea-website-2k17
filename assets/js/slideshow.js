56df46861c7234af76a0255ade6173191887f082
ys1998 <yashshah2398@gmail.com>
2017-06-14 03:27:20 +0530

    Modified script for multiple slideshows on same page

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -3,9 +3,49 @@ function showSlide(n,k) {
   var containers=document.getElementsByClassName("slideshow-container");
   var slides=containers[k-1].getElementsByClassName("slide");
   var dots=containers[k-1].getElementsByClassName("dot");
+
   if(n=='n') {
     for(var i=0;i<dots.length;i++) {
-      if(dots[i].className)
+      if(dots[i].className.includes("dot-active")) {
+        current_slide=i+1;
+        break;
+      }
     }
+    var next_slide;
+    if(current_slide==dots.length) { next_slide=1; }
+    else { next_slide=current_slide+1; }
+    /* Changing class-names of slide and dot */
+    dots[current_slide-1].className="dot";
+    slides[current_slide-1].style.display="none";
+
+    dots[next_slide-1].className += " dot-active";
+    slides[next_slide-1].style.display="block"; 
+
   }
+  else if(n=='p') {
+    for(var i=0;i<dots.length;i++) {
+      if(dots[i].className.includes("dot-active")) {
+        current_slide=i+1;
+        break;
+      }
+    }
+    var prev_slide;
+    if(current_slide==1) { prev_slide=dots.length; }
+    else { prev_slide=current_slide-1; }
+    /* Changing class-names of slide and dot */
+    dots[current_slide-1].className="dot";
+    slides[current_slide-1].style.display="none";
+
+    dots[prev_slide-1].className += " dot-active";
+    slides[prev_slide-1].style.display="block"; 
+
+  }
+  else {
+    for(var i=0;i<dots.length;i++) {
+      dots[i].className="dot";
+      slides[i].style.display="none";
+  }
+  dots[n-1].className += " dot-active";
+  slides[n-1].style.display="block";
+}
 }
