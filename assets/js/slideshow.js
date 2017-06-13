931bec0558765605feb188e113ace3ea1220ad03
ys1998 <yashshah2398@gmail.com>
2017-06-14 01:37:11 +0530

    Gallery webpage added

---
+++ assets/js/slideshow.js
@@ -0,0 +1,11 @@
+function showSlide(n,k) {
+  var current_slide;
+  var containers=document.getElementsByClassName("slideshow-container");
+  var slides=containers[k-1].getElementsByClassName("slide");
+  var dots=containers[k-1].getElementsByClassName("dot");
+  if(n=='n') {
+    for(var i=0;i<dots.length;i++) {
+      if(dots[i].className)
+    }
+  }
+}
